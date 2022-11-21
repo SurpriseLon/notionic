9b70e51ce3a84024544fe5258b922b7869bd9fd9
SurpriseLon <yul62788@gmail.com>
2022-11-21 21:32:25 +0800

    modified:   blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -53,7 +53,7 @@ const BLOG = {
   },
   comment: {
     // support provider: utterances, supacomments
-    provider: 'supacomments', // leave it empty if you don't need any comment plugin
+    provider: 'walinecomponent', // leave it empty if you don't need any comment plugin
     supaCommentsConfig: {
       supabaseUrl: 'https://hcfkgorbxbmrqjosshhk.supabase.co', // The url of your Supabase instance
       supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjZmtnb3JieGJtcnFqb3NzaGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg5NDIxMTEsImV4cCI6MTk4NDUxODExMX0.I4XdJidx436jLzQpNZMRQ7Aah5pVA-UmQmAFPN4Kfgc' // The anonymous key of your Supabase instance
